fe9d6e2e286a098788c8bf2405c2c3b21322ba8f
riddsep <ridoseptiawan00@gmail.com>
2024-12-07 19:55:09 +0700

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -98,24 +98,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats({ items }) {
-  if (!items.length)
-    return (
-      <footer className="stats">
-        Start adding some items yout packing list
-      </footer>
-    );
-  const itemQuantity = items.length;
-  const itemPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((itemPacked / itemQuantity) * 100);
-  return (
-    <footer className="stats">
-      {percentage === 100
-        ? "You got everything! Ready to go ✈️"
-        : `You have ${itemQuantity} items on your list, and you already packed
-      ${itemPacked} (${percentage})%`}
-    </footer>
-  );
-}
+// function Stats({ items }) {
+//   if (!items.length)
+//     return (
+//       <footer className="stats">
+//         Start adding some items yout packing list
+//       </footer>
+//     );
+//   const itemQuantity = items.length;
+//   const itemPacked = items.filter((item) => item.packed).length;
+//   const percentage = Math.round((itemPacked / itemQuantity) * 100);
+//   return (
+//     <footer className="stats">
+//       {percentage === 100
+//         ? "You got everything! Ready to go ✈️"
+//         : `You have ${itemQuantity} items on your list, and you already packed
+//       ${itemPacked} (${percentage})%`}
+//     </footer>
+//   );
+// }
 
 export default App;
