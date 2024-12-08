ac54810b34e110192241e908dde84a7535e86914
riddsep <ridoseptiawan00@gmail.com>
2024-12-08 07:57:01 +0700

    feat: move components into seperate file

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Item from "./Item";
+
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items.slice().sort((a, b) => +a.packed - +b.packed);
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sorted by input order</option>
+          <option value="description">Sorted by desctiption</option>
+          <option value="packed">Sorted by packed status</option>
+        </select>
+      </div>
+      <button onClick={onClearList}>Clear list</button>
+    </div>
+  );
+}
+
+export default PackingList;
